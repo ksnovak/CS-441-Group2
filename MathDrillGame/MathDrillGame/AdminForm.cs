151285f53052107c601e70b8a809ca8c03306d07
Kevin Novak <kev@ksnovak.com>
2014-02-17 21:44:25 -0800

    Release 0.1
    Two user classes. Admins can generate problems, students can do the ones
    assigned to them.
    Users are hardcoded, problems are stored in a list.
    
    Problems:
    1. When in Admin/User screens, need to manually click "Log out" and then
    exit the login form. Else login form doesn't properly exit. (Open up
    task manager and terminate MathDrillGame.exe if need to)
    2. Admin -  the list of users should only have students
    3. Student - Infinite loop in searching for unfinished problem.
    4. Student - If no problem set assigned, error. (due to it attempting to
    find a problem. Just do if list length == 0, return)
    5. Admin - No input validation for min/max/quantity
    
    Note: Only admin is Bill J. Admin.

---
+++ MathDrillGame/MathDrillGame/AdminForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathDrillGame
+{
+    public partial class AdminForm : Form
+    {
+        int min; //Minimum range for RNG
+        int max; //Maximum range for RNG
+        bool isAddition; //Whether the problems are addition or subtraction for RNG
+        bool isAppending; //Whether the problem set is appended or replaces the student's current 
+        static Random rng = new Random(); //Used for generating random numbers. Creates a random seed so that it is more random.
+        public AdminForm()
+        {
+            InitializeComponent();
+        }
+
+        //When the form is opened, give a personalized greeting, and fill the user list with users.
+        private void AdminForm_Load(object sender, EventArgs e)
+        {
+            labelWelcome.Text = "Welcome, " + Program.users[Program.currentUser].fullName;
+            listOfStudents.DataSource = Program.users;
+            listOfStudents.DisplayMember = "fullName"; //This is the value to show on-screen
+            listOfStudents.ValueMember = "userID"; //This is the value to pass
+        }
+
+        //When they click the Logout button, close the Admin form and show the Login form (form1).
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            var form1 = (Form1)Tag;
+            form1.Show();
+            Close();
+        }
+
+        //When you select someone from the list, save to a variable which member it is, and personalize a message.
+        private void listOfStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Program.targetUser = Convert.ToInt32(listOfStudents.SelectedIndex); 
+            labelGenProblemsFor.Text = "Generating problems for " + Program.users[Program.targetUser].fullName;
+
+            //Do something to prevent them from generating problems for admins.
+            if (!Program.users[Program.targetUser].isAdmin)
+            {
+
+            }
+        }
+
+        //When they click the Generate button, start generating problems...
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            //Make sure every input field has a value. If not, don't bother.
+            if (inputMin.Text.Length == 0 || inputMax.Text.Length == 0 || inputQuantity.Text.Length == 0)
+            {
+                return;
+            }
+
+            //Otherwise, there are values in everything. So create a problem.
+            else
+            {
+                min = Convert.ToInt32(inputMin.Text); //Turn the string input into ints
+                max = Convert.ToInt32(inputMax.Text); //Turn the string input into ints
+                if (min > max) //If users mistook min for max, swap the values.
+                {
+                    int temp = min;
+                    min = max;
+                    max = temp;
+                }
+                int quantity = Convert.ToInt32(inputQuantity.Text); //Turn the string input into ints
+                isAddition = radioAddition.Checked; //Determine the type of problem
+                isAppending = radioAppend.Checked; //Determine whether the sets are added to replacing any prior sets.
+
+                if (!isAppending) //If they are replacing, just empty out any current problems assigned to the student.
+                {
+                    Program.users[Program.targetUser].problemSet.Clear();
+                }
+
+                //For as many problems as is necessary, generate a problem and add to the specific student's problem set.
+                for (int i = 0; i < quantity; i++)
+                {
+                    Program.users[Program.targetUser].problemSet.Add(generateProblem());
+                }
+
+                //Once finished generating, display just for the teacher's knowledge.
+                textBox1.Text = "Problem set: \r\n"; 
+                foreach (Problem problem in Program.users[Program.targetUser].problemSet)
+                {
+                    textBox1.AppendText(problem.printProblem() + "\r\n");
+                }
+            }
+        }
+
+        //Generates the problems.
+        //rng is an object of class Random declared at the start of this class
+        private Problem generateProblem()
+        {
+            Problem problem = new Problem();
+            problem.operand1 = rng.Next(min, max); 
+            problem.operand2 = rng.Next(min, max);
+            problem.operation = isAddition; //True = addition selected (default), False = subtraction selected.
+            if (problem.operation)
+                problem.solution = problem.operand1 + problem.operand2;
+            else
+                problem.solution = problem.operand1 - problem.operand2;
+
+            return problem;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    } //end AdminForm class
+} //end namespace
---
+++ MathDrillGame/MathDrillGame/StudentForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace MathDrillGame
+{
+    partial class StudentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelWelcome = new System.Windows.Forms.Label();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.inputAnswer = new System.Windows.Forms.TextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.labelFeedback = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelWelcome
+            // 
+            this.labelWelcome.AutoSize = true;
+            this.labelWelcome.Location = new System.Drawing.Point(115, 9);
+            this.labelWelcome.Name = "labelWelcome";
+            this.labelWelcome.Size = new System.Drawing.Size(0, 13);
+            this.labelWelcome.TabIndex = 0;
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonLogout.Location = new System.Drawing.Point(382, 350);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(90, 43);
+            this.buttonLogout.TabIndex = 0;
+            this.buttonLogout.TabStop = false;
+            this.buttonLogout.Text = "Log out";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 46);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(107, 201);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TabStop = false;
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.labelQuestion.Location = new System.Drawing.Point(149, 129);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(117, 29);
+            this.labelQuestion.TabIndex = 4;
+            this.labelQuestion.Text = "55 + 55 = ";
+            // 
+            // inputAnswer
+            // 
+            this.inputAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.inputAnswer.Location = new System.Drawing.Point(261, 126);
+            this.inputAnswer.Name = "inputAnswer";
+            this.inputAnswer.Size = new System.Drawing.Size(68, 35);
+            this.inputAnswer.TabIndex = 1;
+            this.inputAnswer.TextChanged += new System.EventHandler(this.inputAnswer_TextChanged);
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonSubmit.Location = new System.Drawing.Point(344, 127);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(86, 34);
+            this.buttonSubmit.TabIndex = 2;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // labelFeedback
+            // 
+            this.labelFeedback.AutoSize = true;
+            this.labelFeedback.Enabled = false;
+            this.labelFeedback.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelFeedback.Location = new System.Drawing.Point(191, 199);
+            this.labelFeedback.Name = "labelFeedback";
+            this.labelFeedback.Size = new System.Drawing.Size(0, 20);
+            this.labelFeedback.TabIndex = 7;
+            // 
+            // StudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 405);
+            this.Controls.Add(this.labelFeedback);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.inputAnswer);
+            this.Controls.Add(this.labelQuestion);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.labelWelcome);
+            this.Name = "StudentForm";
+            this.Text = "StudentForm";
+            this.Load += new System.EventHandler(this.StudentForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWelcome;
+        private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.TextBox inputAnswer;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.Label labelFeedback;
+    }
+}
