83a82c336923ae962bae6af2ae04f84661c80a60
Kevin Novak <kev@ksnovak.com>
2014-03-02 11:20:19 -0800

    Release 1.3.4 - Slight fix to new user creation
    Window now closes after submitting.
    ISSUE: Admin form still does not update afterwards.

--- MathDrillGame/MathDrillGame/NewUserForm.Designer.cs
+++ MathDrillGame/MathDrillGame/NewUserForm.Designer.cs
@@ -30,17 +30,17 @@
         {
             this.buttonSaveUser = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.labelName = new System.Windows.Forms.Label();
             this.labelAdmin = new System.Windows.Forms.Label();
             this.checkAdmin = new System.Windows.Forms.CheckBox();
             this.inputFullName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonSaveUser
             // 
             this.buttonSaveUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.buttonSaveUser.Location = new System.Drawing.Point(152, 234);
+            this.buttonSaveUser.Location = new System.Drawing.Point(155, 200);
             this.buttonSaveUser.Name = "buttonSaveUser";
             this.buttonSaveUser.Size = new System.Drawing.Size(121, 42);
             this.buttonSaveUser.TabIndex = 0;
@@ -51,26 +51,18 @@
             // buttonCancel
             // 
             this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.buttonCancel.Location = new System.Drawing.Point(12, 234);
+            this.buttonCancel.Location = new System.Drawing.Point(15, 200);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(121, 42);
             this.buttonCancel.TabIndex = 1;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // labelName
             // 
             this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(9, 52);
+            this.labelName.Location = new System.Drawing.Point(12, 18);
             this.labelName.Name = "labelName";
             this.labelName.Size = new System.Drawing.Size(126, 13);
             this.labelName.TabIndex = 3;
@@ -79,7 +71,7 @@
             // labelAdmin
             // 
             this.labelAdmin.AutoSize = true;
-            this.labelAdmin.Location = new System.Drawing.Point(9, 86);
+            this.labelAdmin.Location = new System.Drawing.Point(12, 52);
             this.labelAdmin.Name = "labelAdmin";
             this.labelAdmin.Size = new System.Drawing.Size(139, 13);
             this.labelAdmin.TabIndex = 4;
@@ -88,7 +80,7 @@
             // checkAdmin
             // 
             this.checkAdmin.AutoSize = true;
-            this.checkAdmin.Location = new System.Drawing.Point(158, 86);
+            this.checkAdmin.Location = new System.Drawing.Point(161, 52);
             this.checkAdmin.Name = "checkAdmin";
             this.checkAdmin.Size = new System.Drawing.Size(44, 17);
             this.checkAdmin.TabIndex = 5;
@@ -97,21 +89,33 @@
             // 
             // inputFullName
             // 
-            this.inputFullName.Location = new System.Drawing.Point(152, 45);
+            this.inputFullName.Location = new System.Drawing.Point(155, 11);
             this.inputFullName.Name = "inputFullName";
             this.inputFullName.Size = new System.Drawing.Size(100, 20);
             this.inputFullName.TabIndex = 6;
             // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(11, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 92);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "BUG WARNING:The admin screen won\'t update with the new user, will have to log out" +
+    " and back in for them to show. Working on fixing this.";
+            // 
             // NewUserForm
             // 
+            this.AcceptButton = this.buttonSaveUser;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(285, 288);
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(285, 252);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.inputFullName);
             this.Controls.Add(this.checkAdmin);
             this.Controls.Add(this.labelAdmin);
             this.Controls.Add(this.labelName);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonSaveUser);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -129,10 +133,10 @@
 
         private System.Windows.Forms.Button buttonSaveUser;
         private System.Windows.Forms.Button buttonCancel;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelName;
         private System.Windows.Forms.Label labelAdmin;
         private System.Windows.Forms.CheckBox checkAdmin;
         private System.Windows.Forms.TextBox inputFullName;
+        private System.Windows.Forms.Label label1;
     }
 }
--- MathDrillGame/MathDrillGame/NewUserForm.cs
+++ MathDrillGame/MathDrillGame/NewUserForm.cs
@@ -19,20 +19,52 @@ namespace MathDrillGame
 
         private void buttonSaveUser_Click(object sender, EventArgs e)
         {
+            addNewUser();
+
+        }
+
+        private void addNewUser()
+        {
+
+            string userName = inputFullName.Text;
+
+
             XmlDocument doc = new XmlDocument();
             doc.Load(@"c:\users\public\MathDrills\users.xml");
             XmlElement newStudent = doc.CreateElement("Student");
-                XmlElement fullName = doc.CreateElement("FullName");
-                    fullName.InnerText = inputFullName.Text;
-                    newStudent.AppendChild(fullName);
-                XmlElement isAdmin = doc.CreateElement("IsAdmin");
-                    isAdmin.InnerText = (checkAdmin.Checked? "1" : "0");
-                    newStudent.AppendChild(isAdmin);
-                XmlElement userID = doc.CreateElement("UserID");
-                    userID.InnerText = "505";
-                    newStudent.AppendChild(userID);
+            XmlElement fullName = doc.CreateElement("FullName");
+            fullName.InnerText = userName;
+            newStudent.AppendChild(fullName);
+            XmlElement isAdmin = doc.CreateElement("IsAdmin");
+            isAdmin.InnerText = (checkAdmin.Checked ? "1" : "0");
+            newStudent.AppendChild(isAdmin);
+            XmlElement userID = doc.CreateElement("UserID");
+            userID.InnerText = "505";
+            newStudent.AppendChild(userID);
             doc.DocumentElement.AppendChild(newStudent);
             doc.Save(@"c:\users\public\MathDrills\users.xml");
+            /*
+                  XElement studentListXML = XElement.Load(@"c:\users\public\MathDrills\users.xml");
+            foreach (XElement user in studentListXML.Descendants("Student"))
+            {
+
+                Program.users.Add(new User { isAdmin = (user.Element("IsAdmin").Value == "1"? true : false), 
+                                            fullName = user.Element("FullName").Value, 
+                                            userID = Convert.ToInt32(user.Element("UserID").Value)
+                                            });
+            }
+             */
+            Program.users.Add(new User { isAdmin = (checkAdmin.Checked ? true : false),
+                                        fullName = userName,
+                                        userID = 505
+            });
+
+            this.Close();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
